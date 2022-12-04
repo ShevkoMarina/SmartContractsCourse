12e1a162730d06aad2d03f16c1872c0c73c79629
ShevkoMarina <shevkomarusya@gmail.com>
2022-12-04 15:05:41 +0300

    Sum contract

---
+++ contracts/AddingNumbersContract.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier; MIT
+pragma solidity ^0.8.4;
+
+contract AddigNumbersConrtract {
+    int a;
+    int b;
+    int c;
+
+    function add(int num_1, int num_2) public {
+        a = num_1;
+        b= num_2;
+        c = a + b;
+    }
+
+    function getSum() public view returns (int) {
+        return c;
+    }
+}
